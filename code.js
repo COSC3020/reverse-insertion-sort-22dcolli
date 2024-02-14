1699b7936a261eaa9335094ef7e70f83392836ad
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-02-14 16:24:18 -0700

    Update code1.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
-function insertionSortReverse(array) {
-    return array;
+function insertionSortReverse(arr) 
+{
+  for(var i = arr.length-1; i > 1; i--) 
+  {
+    var val = arr[i];
+    var j;
+    for(j = i; j > 0 && arr[j-1] > val; j--) {
+      arr[j] = arr[j-1];
+    }
+    arr[j] = val;
+    //console.log(arr)
+  }
+  return arr;
 }
+
+//console.log(insertionSort(arr))
